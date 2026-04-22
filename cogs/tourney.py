4459c348a5d256c6e056eb254924a5423504b8d3
baobuia1 <baobuia1.py@gmail.com>
2026-04-22 18:19:29 +0700

    main

--- cogs/tourney.py
+++ cogs/tourney.py
@@ -1,20 +1,38 @@
+import time
 import random
 import json
 import random
+import asyncio
 import discord
 from discord.ext import commands
+
+class deletion_view(discord.ui.View):
+    @discord.ui.button(label='Confirm ✅')
+    async def confirm_deletion(self,button,interaction):
+        await interaction.channel.delete(reason='Finished match')
+
+
+    @discord.ui.button(label='Cancel ❌')
+    async def cancel_deletion(self,button,interaction):
+        embed = discord.Embed(description='Cancelled deletion')
+        await interaction.respond(embed)
+
+
 
 class tourney(commands.Cog, name='Tourney Commands',guild_ids=[1484925834849681489]):
     def __init__(self,bot):
         self.bot = bot
-    
+
+
     @commands.slash_command(
         name='roll',
         description='Roll a random number from 1 - 100'
     )
     async def roll(self,ctx):
-        await ctx.respond(f'<@{ctx.author.id}> rolled {random.randint(1,100)}')
-    
+        embed = discord.Embed(description=f'<@{ctx.author.id}> rolled {random.randint(1,100)} 🎲')
+        await ctx.respond(embed=embed)
+
+
     @commands.slash_command(
         name='create_room',
         description='Create a channel for a match'
@@ -25,9 +43,9 @@ class tourney(commands.Cog, name='Tourney Commands',guild_ids=[14849258348496814
     async def create(self,ctx,name:str):
         category = ctx.channel.category
         new_channel = await category.create_text_channel(name)
-        with open('./data.json','r') as f:
+        with open('./data.json','r',encoding='utf-8') as f:
             data = json.load(f)
-        with open('./data.json','w') as f:
+        with open('./data.json','w',encoding='utf-8') as f:
             data.append(
                 {
                     'channel_id':new_channel.id,
@@ -38,39 +56,59 @@ class tourney(commands.Cog, name='Tourney Commands',guild_ids=[14849258348496814
                 }
             )
             json.dump(data,f)
-        await ctx.respond(f'Channel created <#{new_channel.id}>')
+        embed = discord.Embed(description=f'Channel created <#{new_channel.id}>')
+        await ctx.respond(embed=embed)
+
+
+    @commands.slash_command(
+        name='end_room',
+        description='End the match and delete channel'
+    )
+    @commands.has_permissions(
+        manage_channels=True
+    )
+    async def end(self,ctx):
+        with open('./data.json','r',encoding='utf-8') as f:
+            data = json.load(f)
+        with open('./data.json','w',encoding='utf-8') as f:
+            data[:] = [d for d in data if d.get('channel_id') != ctx.channel_id]
+            json.dump(data,f)
+        # Add confirmation here
+        confirm_embed = discord.Embed(description='Please confirm that the match has ended')
+        await ctx.respond(f'<@{ctx.author.id}>',embed=confirm_embed,view=deletion_view(timeout=10))
+
 
     @commands.slash_command(
         name='random_maps',
-        description='Randomly choose maps for ban pick ()'
+        description='Randomly choose maps to create a pool'
     )
     async def randomize(self,ctx,num:int,min_diff:float,max_diff:float):
         #check channel
-        with open('./data.json') as fi:
+        with open('./data.json',encoding='utf-8') as fi:
             channel_data = json.load(fi)
         for channel in channel_data:
             if ctx.channel_id == channel['channel_id'] and ctx.author.id in channel['created']:
                 #maps choosing
-                with open('./maps.json') as f:
+                with open('./maps.json',encoding='utf-8') as f:
                     data = json.load(f)
                 qualified = []
                 for x in data:
                     if x['constant'] >= min_diff and x['constant'] <= max_diff:
                         qualified.append(x)
-                print(qualified)
                 chosen_maps = random.sample(qualified,k=num)
-                msg = '```'
+                maps = []
                 for index,chosen_map in enumerate(chosen_maps):
-                    msg += f'{index+1}. {chosen_map['song_name']}[{chosen_map['diff']}] - {chosen_map['constant']}\n'
-                msg+='```'
-                await ctx.respond(msg)
+                    maps.append(discord.Embed(title=f'Map number {index+1}',description=f'{chosen_map['song_name']}[{chosen_map['diff']}]\nConstant: {chosen_map['constant']}'))
+                    msg += f'{index+1}. {chosen_map['song_name']} [{chosen_map['diff']}] - {chosen_map['constant']}\n'
+                await ctx.respond(embeds=maps)
 
                 #data handling
                 for x in channel_data:
                     if x['channel_id'] == ctx.channel_id:
                         x['maps'] = chosen_maps
-                with open('./data.json','w') as fi:
+                with open('./data.json','w',encoding='utf-8') as fi:
                     json.dump(channel_data,fi)
+
 
     @commands.slash_command(
         name='ban',
@@ -78,66 +116,93 @@ class tourney(commands.Cog, name='Tourney Commands',guild_ids=[14849258348496814
     )
     async def ban(self,ctx,map:int):
         map -= 1
-        with open('./data.json') as f:
+        with open('./data.json',encoding='utf-8') as f:
             data = json.load(f)
             for channel in data:
                 if channel['channel_id'] == ctx.channel_id and ctx.author.id in channel['created']:
                     if map in channel['banned']:
-                        await ctx.respond('Cannot ban a banned map')
+                        await ctx.respond(embed=discord.Embed(description='Cannot ban a banned map'))
+                    elif map in channel['picked']:
+                        await ctx.respond(embed=discord.Embed(description='Cannot ban a picked map'))
                     else:
                         channel['banned'].append(map)
-                        await ctx.respond(f'Picked {channel['maps'][map]}')
+                        chosen_map = channel['maps'][map]
+                        await ctx.respond(embed=discord.Embed(description=f'`{map+1}. {chosen_map['song_name']} [{chosen_map['diff']}]` has been banned.'))
         
-        with open('./data.json','w') as f:
+        with open('./data.json','w',encoding='utf-8') as f:
             json.dump(data,f)
-    
+
+
     @commands.slash_command(
         name='pick',
         description='Pick a map'
     )
     async def pick(self,ctx,map:int):
         map -= 1
-        with open('./data.json') as f:
+        with open('./data.json',encoding='utf-8') as f:
             data = json.load(f)
             for channel in data:
                 if channel['channel_id'] == ctx.channel_id and ctx.author.id in channel['created']:
                     if map in channel['banned']:
-                        await ctx.respond('Cannot pick a banned map')
+                        await ctx.respond(embed=discord.Embed(description='Cannot pick a banned map'))
                     elif map in channel['picked']:
-                        await ctx.respond('Cannot pick a picked map')
+                        await ctx.respond(embed=discord.Embed(description='Cannot pick a picked map'))
                     else:
                         channel['picked'].append(map)
-                        await ctx.respond(f'Picked {channel['maps'][map]}')
+                        chosen_map = channel['maps'][map]
+                        await ctx.respond(embed=discord.Embed(description=f'`{map+1}. {chosen_map['song_name']} [{chosen_map['diff']}]` has been banned'))
         
-        with open('./data.json','w') as f:
+        with open('./data.json','w',encoding='utf-8') as f:
             json.dump(data,f)
-    
+
+
     @commands.slash_command(
         name='pool',
         description='Show a pool'
     )
     async def pool(self,ctx):
-        with open('./data.json') as f:
+        with open('./data.json',encoding='utf-8') as f:
             data = json.load(f)
             for channel in data:
                 if channel['channel_id'] == ctx.channel_id:
                     msg = '```'
+                    maps = []
                     for index,chosen_map in enumerate(channel['maps']):
-                        msg += f'{index+1}. {chosen_map['song_name']}[{chosen_map['diff']}] - {chosen_map['constant']}\n'
-                    msg+='```'
-                    await ctx.respond(msg)
-    
+                        #msg += f'{index+1}. {chosen_map['song_artist']} - {chosen_map['song_name']}[{chosen_map['diff']}] - {chosen_map['constant']}\n'
+                        if index not in channel['banned'] and index not in channel['picked']:
+                            maps.append(discord.Embed(title=f'Map number {index+1}',description=f'{chosen_map['song_name']}[{chosen_map['diff']}]\nConstant: {chosen_map['constant']}'))
+                        elif index in channel['picked']:
+                            maps.append(discord.Embed(color=discord.Color.red(),title=f'Map number {index+1} PICKED',description=f'~~{chosen_map['song_name']}[{chosen_map['diff']}]\nConstant: {chosen_map['constant']}~~'))
+                        elif index in channel['banned']:
+                            maps.append(discord.Embed(color=discord.Color.red(),title=f'Map number {index+1} BANNED',description=f'~~{chosen_map['song_name']}[{chosen_map['diff']}]\nConstant: {chosen_map['constant']}~~'))
+                    await ctx.respond(embeds=maps)
+
+
     @commands.slash_command(
         name='add_ref',
         description='Add a referee to the match'
     )
     async def add_ref(self,ctx,ref:discord.Member):
-        with open('./data.json') as f:
+        with open('./data.json',encoding='utf-8') as f:
             data = json.load(f)
             for channel in data:
                 if channel['channel_id'] == ctx.channel_id and ctx.author.id in channel['created']:
                     channel['created'].append(ref.id)
-        with open('./data.json','w') as f:
+        with open('./data.json','w',encoding='utf-8') as f:
+            json.dump(data,f)
+
+
+    @commands.slash_command(
+        name='remove_ref',
+        description='Remove a referee from the match'
+    )
+    async def remove_ref(self,ctx,ref:discord.Member):
+        with open('./data.json',encoding='utf-8') as f:
+            data = json.load(f)
+            for channel in data:
+                if channel['channel_id'] == ctx.channel_id and ctx.author.id in channel['created']:
+                    channel['created'].remove(ref.id)
+        with open('./data.json','w',encoding='utf-8') as f:
             json.dump(data,f)
 
 def setup(bot):
